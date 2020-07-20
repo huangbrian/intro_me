a8338e81636312a8191fc997e213e849d1e2540a
Brian Huang <bt99huang@gmail.com>
2020-07-19 17:23:25 -0700

    Initial Commit

---
+++ introduceme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  introduceme
+//
+//  Created by Brian Huang on 7/19/20.
+//  Copyright © 2020 team51. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
